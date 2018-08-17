1332a395142264ca93ae22783205a776d26b098b
AlastairP <alastairp@jacob_paul@outlook.com>
2018-08-18 00:13:32 +0200

    Program now working

---
+++ Steganography/SteganographyExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace SteganographyExample
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			while (true)
+			{
+				string caset = Console.ReadLine();
+				switch(caset){
+					case "h":{
+							var bit = new Bitmap(Console.ReadLine());
+                            string textLoc = Console.ReadLine();
+							string imName = Console.ReadLine();
+                            
+							var em = Steganography.SteganographyHelper.embedText(File.ReadAllText(textLoc), bit);
+                            
+							em.Save(imName, ImageFormat.Jpeg);
+							break;
+						}
+					case "e":{
+							string file = Console.ReadLine();
+
+							Console.WriteLine(Steganography.SteganographyHelper.extractText(new Bitmap(file)));
+							break;
+						}
+				}
+
+
+			}
+        }
+    }
+}
